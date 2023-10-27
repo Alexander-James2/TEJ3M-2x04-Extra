a7df3cda13174e0a2d6a2f4e0c952989d5f69078
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-10-27 11:23:22 -0400

    near final code

--- code.py
+++ code.py
@@ -6,31 +6,53 @@ import time
 import board
 import digitalio
 
-led_red = digitalio.DigitalInOut(board.GP8)
-led_green = digitalio.DigitalInOut(board.GP7)
-led_blue = digitalio.DigitalInOut(board.GP9)
+led_red = digitalio.DigitalInOut(board.GP7)
+led_green = digitalio.DigitalInOut(board.GP6)
+led_blue = digitalio.DigitalInOut(board.GP8)
 
 led_red.direction = digitalio.Direction.OUTPUT
 led_green.direction = digitalio.Direction.OUTPUT
 led_blue.direction = digitalio.Direction.OUTPUT
 
 
-
 while True:
+    # turns red
     led_red.value = True
     led_green.value = False
     led_blue.value = False
+    time.sleep(1)
 
+    # turns green
+    led_red.value = False
+    led_green.value = True
+    led_blue.value = False
+    time.sleep(1)
+
+    # turns blue
+    led_red.value = False
+    led_green.value = False
+    led_blue.value = True
+    time.sleep(1)
+
+    # turns purple
+    led_red.value = True
+    led_green.value = False
+    led_blue.value = True
+    time.sleep(1)
+
+    # turns ye
+    led_red.value = True
+    led_green.value = True
+    led_blue.value = False
     time.sleep(1)
 
     led_red.value = False
     led_green.value = True
-    led_blue.value = False
-
+    led_blue.value = True
     time.sleep(1)
 
-    led_red.value = False
-    led_green.value = False
+    # turn white
+    led_red.value = True
+    led_green.value = True
     led_blue.value = True
-
-    
+    time.sleep(1)
